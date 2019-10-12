0b81646a235a92a5423d7724ace4e805d5e40be6
Juan Escobar <juand-escobart@unilibre.edu.co>
2019-10-12 18:41:46 -0500

    Termino de microservicio Profes

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO profes (name,apellido,materia) VALUES ('Juan','Henao','Programacion');
+INSERT INTO profes (name,apellido,materia) VALUES ('Juan','Escobar','Ing. Software');
+INSERT INTO profes (name,apellido,materia) VALUES ('Jonathan','Mantilla','Lo que venga');
